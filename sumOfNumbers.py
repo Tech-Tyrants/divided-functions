6740a98f610b5c7b9099103b677e6f26f00993d5
bernard0975 <bernardaboki32@gmail.com>
2023-05-15 13:59:08 +0100

    feat: sum of numbers

---
+++ sumOfNumbers.py
@@ -0,0 +1,7 @@
+def calculate_sum(numbers):
+    total = sum(numbers)
+    return total
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9,10]
+result = calculate_sum(my_list)
+print("The sum of the numbers in my list is:", result)
